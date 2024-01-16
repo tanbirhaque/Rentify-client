108c471e2ac86703aa70ef41c200ccf70003b732
sadiaafrin67 <sa540276@gmail.com>
2024-01-16 22:28:49 +0600

    add my edited half popular section by sadia

--- src/MainLayout/Pages/Home/HomeComponents/PopularCities/PopularCities.jsx
+++ src/MainLayout/Pages/Home/HomeComponents/PopularCities/PopularCities.jsx
@@ -5,7 +5,7 @@ import img4 from "../../../../../assets/Template_files/city-4.jpg";
 import img5 from "../../../../../assets/Template_files/city-5.jpg";
 import img6 from "../../../../../assets/Template_files/city-6.jpg";
 import img7 from "../../../../../assets/Template_files/city-7.jpg";
-import img8 from "../../../../../assets/Template_files/city-8.jpg";
+// import img8 from "../../../../../assets/Template_files/city-8.jpg";
 import "./Popular.css";
 
 const PopularCities = () => {
@@ -19,10 +19,9 @@ const PopularCities = () => {
       </h2>
 
       <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4 px-4">
+            {/* img1 */}
         <div className="relative  popular">
-            <div className="under">
-                
-            </div>
+            <div className="under"></div>
           <div className="overlay  flex items-end rounded-lg transition duration-300 ease-in-out bg-black bg-opacity-50   hover:bg-opacity-60  absolute  ">
 
             <div className="z-10 p-5 text-white font-medium text-base ">
@@ -30,30 +29,67 @@ const PopularCities = () => {
               <p>+5231 properties</p>
             </div>
           </div>
-          {/* <img className="w-full h-full rounded-lg bg-opacity-60 bg-blend-overlay " src={img1} alt="" /> */}
         </div>
+
+        {/* img2 */}
+        <div className="relative  populartwo">
+            <div className="under"></div>
+          <div className="overlay  flex items-end rounded-lg transition duration-300 ease-in-out bg-black bg-opacity-50   hover:bg-opacity-60  absolute  ">
+
+            <div className="z-10 p-5 text-white font-medium text-base ">
+              <h2 className="">New York City</h2>
+              <p>+5231 properties</p>
+            </div>
+          </div>
+        </div>
+
+        {/* img3 */}
+        <div className="relative  popularthree">
+            <div className="under"></div>
+          <div className="overlay  flex items-end rounded-lg transition duration-300 ease-in-out bg-black bg-opacity-50   hover:bg-opacity-60  absolute  ">
+
+            <div className="z-10 p-5 text-white font-medium text-base ">
+              <h2 className="">New York City</h2>
+              <p>+5231 properties</p>
+            </div>
+          </div>
+        </div>
+
+        {/* img4 */}
+        {/* <div className="relative  popularfour">
+            <div className="under"></div>
+          <div className="overlay  flex items-end rounded-lg transition duration-300 ease-in-out bg-black bg-opacity-50   hover:bg-opacity-60  absolute  ">
+
+            <div className="z-10 p-5 text-white font-medium text-base ">
+              <h2 className="">New York City</h2>
+              <p>+5231 properties</p>
+            </div>
+          </div>
+        </div> */}
 
         <div>
           <img className="w-full h-full rounded-lg" src={img2} alt="" />
         </div>
-        <div>
+        {/* <div>
           <img className="w-full h-full rounded-lg" src={img3} alt="" />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <img className="w-full h-full rounded-lg" src={img4} alt="" />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <img className="w-full h-full rounded-lg" src={img5} alt="" />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <img className="w-full h-full rounded-lg" src={img6} alt="" />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <img className="w-full h-full rounded-lg" src={img7} alt="" />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <img className="w-full h-full rounded-lg" src={img8} alt="" />
-        </div>
+        </div> */}
+
+
       </div>
     </div>
   );
