dbe3ec5c5dbd46ac1409563325ce9b30d430f86c
Fahima <sfahima1388@gmail.com>
2024-01-17 11:11:57 +0600

    auth configuartion added by fahima

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ import Register from "./MainLayout/Pages/Register/Register.jsx";
 import Faq from "./MainLayout/Pages/Faq/Faq.jsx";
 import ErrorPage from "./MainLayout/Pages/Error/ErrorPage.jsx";
 import HowItWorks from "./MainLayout/Pages/How-It-Works/HowItWorks.jsx";
+import AuthProvider from "./Provider/AuthProvider.jsx";
 
 const router = createBrowserRouter([
   {
@@ -42,6 +43,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
