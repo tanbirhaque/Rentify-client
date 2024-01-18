081ee0b6f6f22bd10496267717657fcafe1c204d
Tanbir Haque <tanbirhaque53@gmail.com>
2024-01-18 07:15:43 +0600

    Popular cities section on homepage, Initially started by Sadia Afrin, After facing some issues, Re assigned to Tanbir. Finally Developed and made fully responsive by -Tanbir

---
+++ src/MainLayout/Shared/CommonHeading/CommonHeading.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CommonHeading = () => {
+    return (
+        <div>
+            <p className="lg:text-base md:text-sm text-xs font-medium mt-3 mb-[10px] text-[#e33226] text-center">
+                Choose A Popular City
+            </p>
+            <h2 className="poppins-font text-center text-4xl font-bold">
+                Our Hottest Nearby Cities
+            </h2>
+        </div>
+    );
+};
+
+export default CommonHeading;
