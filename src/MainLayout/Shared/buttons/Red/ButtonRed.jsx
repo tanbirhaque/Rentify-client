43278730760051dd6c865cfe7f5448a991ca0204
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-01-21 14:33:33 +0600

    blogs & testimonials page data pagination aded done

--- src/MainLayout/Shared/buttons/Red/ButtonRed.jsx
+++ src/MainLayout/Shared/buttons/Red/ButtonRed.jsx
@@ -4,8 +4,8 @@ const ButtonRed = ({ titleRed }) => {
   return (
     <>
     {/* removed mb-2 tailwind class from the button tag by -Tanbir for design purpose */}
-      <button className="w-full h-full p-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#Ec3323] text-white relative overflow-hidden group z-10 hover:text-white duration-1000">
-        <span className="absolute  bg-[#002172] w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-900 duration-500 origin-center transform transition-all"></span>
+      <button className="w-full h-full p-4 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-yellow-500 text-white relative overflow-hidden group z-10 hover:text-white duration-1000">
+        <span className="absolute  bg-black w-36 h-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-900 duration-500 origin-center transform transition-all"></span>
         {titleRed}
       </button>
     </>
