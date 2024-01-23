234fa8389edbe241a5e7a5e43ffd0dfd658cc7d6
sadiaafrin67 <sa540276@gmail.com>
2024-01-23 18:55:44 +0600

    add tabs on profile route by sadia

--- src/DashboardRoutes/Profile/Profile.jsx
+++ src/DashboardRoutes/Profile/Profile.jsx
@@ -1,43 +1,15 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Profile.css";
 
 const Profile = () => {
   return (
-    //     <div className="holder  ">
-    //   <div  className="card border w-[1350px] hover:shadow-none relative flex flex-col mx-auto shadow-lg m-5 h-[400px]">
-    //     <img
-    //       className="max-h-20 w-full h-[300px] opacity-80 absolute top-0"
-    //       style={{ zIndex: -1 }}
-    //       src="https://unsplash.com/photos/h0Vxgz5tyXA/download?force=true&w=640"
-    //       alt=""
-    //     />
-    //     <div className="profile w-full flex m-3 ml-4 text-white">
-    //       <img
-    //         className="w-28 h-28 p-1 bg-white rounded-full"
-    //         src="https://images.pexels.com/photos/61100/pexels-photo-61100.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb"
-    //         alt=""
-    //       />
-    //       <div className="title mt-11 ml-3 font-bold flex flex-col">
-    //         <div className="name break-words">Sarah</div>
-    //         {/*  add [dark] class for bright background */}
-    //         <div className="add font-semibold text-sm italic dark">Model</div>
-    //       </div>
-    //     </div>
-    //     <div className="buttons flex absolute bottom-0 font-bold right-0 text-xs text-gray-500 space-x-0 my-3.5 mr-3">
-    //       <div className="add border rounded-l-2xl rounded-r-sm border-gray-300 p-1 px-4 cursor-pointer hover:bg-gray-700 hover:text-white">
-    //         Contact
-    //       </div>
-    //       {/* <div className="add border rounded-r-2xl rounded-l-sm border-gray-300 p-1 px-4 cursor-pointer hover:bg-gray-700 hover:text-white">Bio</div> */}
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="holder w-[1350px]  mx-auto">
-      <div className="card border h-[400px]  hover:shadow-none relative flex flex-col mx-auto m-5">
+      <div className="card  h-[400px] border-t  hover:shadow-none relative flex flex-col mx-auto m-5">
         <div className="w-full ">
           <img
-            className=" w-full  h-[200px]  opacity-80"
+            className=" w-full rounded-t-lg h-[200px]  opacity-80"
             style={{ zIndex: -1 }}
-            src="https://i.ibb.co/SvDkgSC/03.png"
+            src="https://dash-ui-admin-template.vercel.app/images/background/profile-cover.jpg?fbclid=IwAR33hMTT9POzg0spN-quLZSjRPLq_oZTMeyWR3I3Zjt9Jg7R-5ZEv8XsaYY"
             alt=""
           />
         </div>
@@ -59,7 +31,7 @@ const Profile = () => {
             <div className="text-end -mt-16">
               <button
                 type="button"
-                className="text-gray-800 hover:text-white hover:bg-gray-800 hover:border-gray-800 border border-gray-800 text-base  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5 text-center me-2 mb-2 "
+                className="text-gray-800 hover:text-white hover:bg-gray-800 hover:border-gray-800 border border-gray-800 text-base  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  px-5 py-2.5 text-center me-2 mb-2 mr-7"
               >
                 Edit Profile
               </button>
@@ -69,34 +41,43 @@ const Profile = () => {
       </div>
 
       <div>
-        <nav className="flex" aria-label="Navigation">
-          <ul className="flex space-x-4">
-            <li className="list-none">
-              <a href="/#" className="text-blue-500" aria-current="page">
-                Active
-              </a>
-            </li>
-            <li className="list-none">
-              <a href="/#" className="text-blue-500">
-                Link
-              </a>
-            </li>
-            <li className="list-none">
-              <a href="/#" className="text-blue-500">
-                Link
-              </a>
-            </li>
-            <li className="list-none">
-              <a
-                href="/#"
-                className="text-gray-400 cursor-not-allowed"
-                aria-disabled="true"
-              >
-                Disabled
-              </a>
-            </li>
+        <div className="text-sm font-medium text-center text-gray-500 border-b border-t border-gray-200 dark:text-gray-400 dark:border-gray-700">
+          <ul className="flex flex-wrap mb-5 mt-5 ">
+            {/* <li className="me-2">
+      <a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
+    </li>
+    <li className="me-2">
+      <a  className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active " aria-current="page">Dashboard</a>
+    </li>
+    <li className="me-2">
+      <a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Settings</a>
+    </li>
+    <li className="me-2">
+      <a href="#" className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Contacts</a>
+    </li>
+    <li>
+      <a className="inline-block p-4 text-gray-400 rounded-t-lg cursor-not-allowed dark:text-gray-500">Disabled</a>
+    </li> */}
+            <NavLink
+              to=""
+              className="picAfter relative font-medium text-base text-black mx-3"
+            >
+              Blogs
+            </NavLink>
+            <NavLink
+              to=""
+              className="picAfter relative font-medium text-base text-black mx-3"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to=""
+              className="picAfter relative font-medium text-base  text-black mx-3"
+            >
+              How It Works
+            </NavLink>
           </ul>
-        </nav>
+        </div>
       </div>
     </div>
   );
