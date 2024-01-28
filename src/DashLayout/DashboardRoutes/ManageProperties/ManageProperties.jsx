39042ed2a2faa61adccfa03c70409dc27bcd0c57
mrana007 <rana.es.bc@gmail.com>
2024-01-28 22:49:55 +0600

    implement manage properties of owner by [rana]

--- src/DashLayout/DashboardRoutes/ManageProperties/ManageProperties.jsx
+++ src/DashLayout/DashboardRoutes/ManageProperties/ManageProperties.jsx
@@ -1,11 +1,44 @@
-
+// implement by Rana;
+import { useState } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import PropertyCard from "../../../MainLayout/Shared/PropertyCards/PropertyCard";
+import useProperties from "../../../Hooks/useProperties";
 
 const ManageProperties = () => {
-    return (
-        <div>
-            
+  const [tabIndex, setTabIndex] = useState(0);
+  const [properties] = useProperties();
+
+  const Pending = properties?.filter((item) => item?.property_info.verify_status === false);
+  const Verified = properties?.filter((item) => item.property_info.verify_status === true);
+
+  return (
+    <div className="p-6">
+      <Tabs defaultIndex={tabIndex} onSelect={(i) => setTabIndex(i)} >
+      <TabList>
+        <Tab>Pending Properties</Tab>
+        <Tab>Verified Properties</Tab>
+      </TabList>
+      <TabPanel>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 xl:px-0">
+          {Pending?.map((property, index) => (
+            <div key={index}>
+              <PropertyCard property={property} details_path={"/details"} />
+            </div>
+          ))}
         </div>
-    );
+      </TabPanel>
+      <TabPanel>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 xl:px-0">
+          {Verified?.map((property, index) => (
+            <div key={index}>
+              <PropertyCard property={property} details_path={"/details"} />
+            </div>
+          ))}
+        </div>
+      </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ManageProperties;
