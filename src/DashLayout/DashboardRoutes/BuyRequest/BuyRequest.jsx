2503ff07622736c0fdbbaae2d5fc73942e585f4c
konika098 <konikaakter2000@gmail.com>
2024-01-29 03:44:33 +0600

    dynamic Buy Request and rent req route by konika

---
+++ src/DashLayout/DashboardRoutes/BuyRequest/BuyRequest.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import useOwnerSale from "../../../Hooks/useOwnerSale";
+import BuyReqCard from "./BuyReqCard";
+const BuyRequest = () => {
+    const [ownerSale]=useOwnerSale()
+    console.log(ownerSale);
+    return (
+        <>
+        {
+                ownerSale.map(item => ( 
+                    <BuyReqCard key={item._id} item={item}></BuyReqCard>
+                ))
+            }
+        </>
+    );
+};
+
+export default BuyRequest;
