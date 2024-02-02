4f51b1a0a1181998b3931061027036459575550a
konika098 <konikaakter2000@gmail.com>
2024-02-02 15:54:14 +0600

    dynamicly data fetch on rentOut and soldOut routes by konika

--- src/DashLayout/DashboardRoutes/RentOutProperties/RentOutProperties.jsx
+++ src/DashLayout/DashboardRoutes/RentOutProperties/RentOutProperties.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import useRentOutProperties from '../../../Hooks/useRentOutProperties';
 
 const RentOutProperties = () => {
+    const [rentOutProperties] = useRentOutProperties();
+    console.log(rentOutProperties);
+
     return (
-        <div>
-            <h2>hiiiiiiiiiiii</h2>
-        </div>
+        <>
+            <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                    {/* head */}
+                    <thead className="font-semibold text-lg text-[#002172]">
+                        <tr>
+                            <th>Renter-Email</th>
+                            <th>Price</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                            <th>transactionId</th>
+                           
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rentOutProperties.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.paymentUser}</td>
+                                <td>{item.price}</td>
+                                <td>{item.date}</td>
+                                <td className='text-[#e33226]'>{item.property_status}</td>
+                                <td>{item.transactionId}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 };
 
