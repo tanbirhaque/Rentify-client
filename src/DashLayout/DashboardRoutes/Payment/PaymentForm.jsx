5eaf01f34ce5aa34840a08fe691c48198cac1ab7
mrana007 <rana.es.bc@gmail.com>
2024-02-06 01:21:52 +0600

    make responsive paymenCard by Rana

--- src/DashLayout/DashboardRoutes/Payment/PaymentForm.jsx
+++ src/DashLayout/DashboardRoutes/Payment/PaymentForm.jsx
@@ -3,25 +3,34 @@ import { FaAffiliatetheme, FaCcAmazonPay, FaMoneyCheck } from "react-icons/fa";
 import { SiKlarna } from "react-icons/si";
 import { useEffect, useState } from "react";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
-import useAuth from '../../../Hooks/useAuth';
+import useAuth from "../../../Hooks/useAuth";
 import Swal from "sweetalert2";
 
-const PaymentForm = ({ queryParams, }) => {
+const PaymentForm = ({ queryParams }) => {
   const [error, setError] = useState();
   const { user } = useAuth();
-  const [clientSecret, setClientSecret] = useState('');
-  const [transactionId, setTransactionId] = useState('');
+  const [clientSecret, setClientSecret] = useState("");
+  const [transactionId, setTransactionId] = useState("");
   const stripe = useStripe();
   const elements = useElements();
   const axiosSecure = useAxiosSecure();
-  const { price, requestId, propertyId, owner, property_status, property_img, property_title } = queryParams;
+  const {
+    price,
+    requestId,
+    propertyId,
+    owner,
+    property_status,
+    property_img,
+    property_title,
+  } = queryParams;
 
   useEffect(() => {
-    axiosSecure.post('/create-payment-intent', { price, requestId, propertyId, owner })
-      .then(res => {
-        console.log(res.data.clientSecret)
-        setClientSecret(res.data.clientSecret)
-      })
+    axiosSecure
+      .post("/create-payment-intent", { price, requestId, propertyId, owner })
+      .then((res) => {
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret);
+      });
   }, [axiosSecure, price, requestId, propertyId, owner]);
 
   const handleSubmit = async (event) => {
@@ -43,25 +52,25 @@ const PaymentForm = ({ queryParams, }) => {
       setError(error.message);
     } else {
       console.log("payment method", paymentMethod);
-      setError('');
+      setError("");
     }
     // confirm error
-    const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: card,
-        billing_details: {
-          email: user?.email || "anonymous",
-          name: user?.displayName || "anonymous"
-        }
-      }
-    })
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
     if (confirmError) {
-      console.log('confirm error');
-    }
-    else {
-      console.log('payment intent', paymentIntent);
+      console.log("confirm error");
+    } else {
+      console.log("payment intent", paymentIntent);
       if (paymentIntent.status === "succeeded") {
-        console.log('transaction id', paymentIntent.id);
+        console.log("transaction id", paymentIntent.id);
         setTransactionId(paymentIntent.id);
         // save the payment in the database
         const payment = {
@@ -74,7 +83,7 @@ const PaymentForm = ({ queryParams, }) => {
           date: new Date().toLocaleDateString("en-GB"),
           transactionId: paymentIntent.id,
         };
-        const res = axiosSecure.post('/payments', payment);
+        const res = axiosSecure.post("/payments", payment);
         if (res) {
           Swal.fire({
             position: "top-end",
@@ -89,120 +98,135 @@ const PaymentForm = ({ queryParams, }) => {
   };
 
   return (
-    <div className="px-10">
+    <div className="px-4 md:px-10">
       {/* new from added and desgin by sojib */}
       <div>
-        <div className=" flex justify-center items-center my-10">
-          <div className=" bg-gray-800 w-[440px] h-[780px] rounded-l-lg">
-            <div className=" ml-10 mt-10">
+        <div className=" flex flex-col md:flex-row justify-center items-center my-10">
+          <div className="bg-gray-800 w-full md:w-[440px] h-auto md:h-[780px] rounded-t-lg md:rounded-r-none md:rounded-l-lg">
+            <div className="ml-4 md:mt-10 mt-5">
               <div className="flex mb-2 items-center">
                 <img
                   className="max-w-[60px] md:w-full"
                   src="https://i.ibb.co/3kqdMYy/logo-white.png"
+                  alt="Logo"
                 />
                 <h4 className="font-bold poppins-font text-2xl lg:text-[34px] text-white ml-2">
                   Renti<span className="text-[#e33226]">fy</span>
                 </h4>
               </div>
-              <h2 className=" text-white font-bold mb-1"><span className=" text-3xl text-white font-bold">P</span>roperty</h2>
-              <p className=" my-4 text-white">{property_title}</p>
-              <p className=" text-2xl font-bold text-white">{price} $</p>
-              <div>
-                <img className=" w-[75%] rounded mt-2 h-[260px]" src={property_img} alt="" />
+              <h2 className="text-white font-bold mb-1 md:text-xl lg:text-3xl">
+                <span className="text-3xl font-bold">P</span>roperty
+              </h2>
+              <p className="my-2 text-white text-sm md:text-base lg:text-lg">
+                {property_title}
+              </p>
+              <p className="text-2xl font-bold text-white">$ {price}</p>
+              <div className="pr-4">
+                <img
+                  className="w-full rounded mt-2 h-[260px]"
+                  src={property_img}
+                  alt="Property"
+                />
               </div>
-              <p className=" text-sm mt-20 text-gray-50">Powred by stripe   <span>terms & conditions</span></p>
+              <p className="text-sm text-center mt-5 pb-4 text-gray-50">
+                Powered by stripe <span className="text-white">|</span>{" "}
+                <span className="text-white">Terms & Conditions</span>
+              </p>
             </div>
           </div>
-          <div className=" bg-gray-100 rounded-r-lg w-[580px] h-[780px]">
+          <div className="bg-gray-100 rounded-b-lg md:rounded-l-none md:rounded-r-lg w-full md:w-[580px] h-auto md:h-[780px]">
             <form className="card-body mt-8" onSubmit={handleSubmit}>
               <div className="w-[80%] mx-auto">
-                <h2 className=" text-3xl font-bold">Pay For properties</h2>
+                <h2 className="text-3xl font-bold">Pay For Properties</h2>
               </div>
               <div className="form-control w-[80%] mx-auto">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
-                <input type="email" placeholder={user.email} className="input input-bordered " readOnly />
+                <input
+                  type="email"
+                  placeholder={user?.email}
+                  className="input input-bordered"
+                  readOnly
+                />
               </div>
               <div className="form-control w-[80%] mx-auto">
                 <label className="label">
                   <span className="label-text">Phone Number</span>
                 </label>
-                <input type="number" placeholder="Phone Number" className="input input-bordered" />
-              </div>
-              <div className="form-control w-[80%] mx-auto mt-5">
-                <label className="label">
-                  <span className="label-text">Additional Number</span>
-                </label>
-                <input type="text" placeholder="Number" className="input input-bordered" />
+                <input
+                  type="number"
+                  placeholder="Phone Number"
+                  className="input input-bordered"
+                />
               </div>
               <div className="">
                 <label className="label w-[80%] mx-auto">
-                  <span className=" text-xl font-bold">Payment method</span>
+                  <span className="text-xl font-bold">Payment Method</span>
                 </label>
-                <div className=" flex justify-evenly items-center w-[80%] mx-auto">
-                  <div className=" border-2 p-2 rounded-lg w-[100px]">
-                    <FaMoneyCheck className=" text-xl font-bold"></FaMoneyCheck>
-                    <span className=" mt-1">Card</span>
+                <div className="flex flex-wrap justify-evenly items-center w-[80%] mx-auto">
+                  <div className="border-2 p-2 rounded-lg w-[100px]">
+                    <FaMoneyCheck className="text-xl font-bold"></FaMoneyCheck>
+                    <span className="mt-1">Card</span>
                   </div>
-                  <div className=" border-2 p-2 rounded-lg w-[100px]">
-                    <FaCcAmazonPay className=" text-xl font-bold"></FaCcAmazonPay>
-                    <span className=" mt-1">Apple Pay</span>
+                  <div className="border-2 p-2 rounded-lg w-[100px]">
+                    <FaCcAmazonPay className="text-xl font-bold"></FaCcAmazonPay>
+                    <span className="mt-1">Apple Pay</span>
                   </div>
-                  <div className=" border-2 p-2 rounded-lg w-[100px]">
-                    <SiKlarna className=" text-xl font-bold"></SiKlarna>
-                    <span className=" mt-1">Klarna</span>
+                  <div className="border-2 p-2 rounded-lg w-[100px]">
+                    <SiKlarna className="text-xl font-bold"></SiKlarna>
+                    <span className="mt-1">Klarna</span>
                   </div>
-                  <div className=" border-2 p-2 rounded-lg w-[100px]">
-                    <FaAffiliatetheme className=" text-xl font-bold"></FaAffiliatetheme>
-                    <span className=" mt-1">Affirm</span>
+                  <div className="border-2 p-2 rounded-lg w-[100px]">
+                    <FaAffiliatetheme className="text-xl font-bold"></FaAffiliatetheme>
+                    <span className="mt-1">Affirm</span>
                   </div>
                 </div>
-                <div className=" w-[80%] mx-auto my-5">
+                <div className="w-[80%] mx-auto my-5">
                   <label className="label">
-                    <span className="label-text font-bold">Card Information</span>
+                    <span className="label-text font-bold">
+                      Card Information
+                    </span>
                   </label>
+                  {/* CardElement component */}
                   <CardElement
-                    className=" w-[98%] h-5 mx-auto"
+                    className="w-full h-5 mx-auto"
                     options={{
                       style: {
                         base: {
-                          fontSize: '28px',
-                          color: 'black',
-                          '::placeholder': {
-                            color: 'gray',
+                          fontSize: "28px",
+                          color: "black",
+                          "::placeholder": {
+                            color: "gray",
                           },
                         },
                         invalid: {
-                          color: '#9e2146',
+                          color: "#9e2146",
                         },
                       },
                     }}
                   />
                 </div>
-
-
               </div>
               <div className="form-control mt-3 w-[80%] mx-auto">
-                <button className="btn bg-[#002172] my-3 text-white" type="submit"
-                  disabled={!stripe || !clientSecret}>
+                <button
+                  className="btn bg-[#002172] my-3 text-white w-full"
+                  type="submit"
+                  disabled={!stripe || !clientSecret}
+                >
                   Pay for: $ {price} {owner}
                 </button>
                 <p className="text-red-600">{error}</p>
-                {transactionId && <p className="text-green-800"> Your transaction id: {transactionId}</p>}
+                {transactionId && (
+                  <p className="text-green-800">
+                    Your transaction id: {transactionId}
+                  </p>
+                )}
               </div>
             </form>
           </div>
         </div>
       </div>
-      {/* <form onSubmit={handleSubmit}>
-        <button className="btn btn-info my-3" type="submit"
-          disabled={!stripe || !clientSecret}>
-          Pay for: $ {price} {owner}
-        </button>
-        <p className="text-red-600">{error}</p>
-        {transactionId && <p className="text-green-800"> Your transaction id: {transactionId}</p>}
-      </form> */}
     </div>
   );
 };
