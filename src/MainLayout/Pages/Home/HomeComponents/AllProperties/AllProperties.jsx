63f6ecc9f662c35cd80cc2eefa1fef8cdd998664
sadiaafrin67 <sa540276@gmail.com>
2024-02-09 03:36:30 +0600

    add my search field code

--- src/MainLayout/Pages/Home/HomeComponents/AllProperties/AllProperties.jsx
+++ src/MainLayout/Pages/Home/HomeComponents/AllProperties/AllProperties.jsx
@@ -1,3 +1,5 @@
+// This section is created by Sadia
+ 
 import { NavLink } from "react-router-dom";
 import "./all.css";
 import useProperties from "../../../../../Hooks/useProperties";
@@ -6,8 +8,11 @@ import DynamicCards from "../PopularCities/DynamicCards";
 const AllProperties = () => {
   const [properties] = useProperties();
 
+  
+
   return (
     <div>
+        {/* for bannner */}
       <div className="allimg">
         <div className=" bg-[#000000B2]">
           <div className=" max-w-screen-2xl mx-auto py-24">
@@ -32,8 +37,56 @@ const AllProperties = () => {
         </div>
       </div>
 
+       {/* for search field */}
+      <div className="max-w-screen-xl mx-auto gap-4 mt-16">
+        <form>
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Please search by property title"
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute end-2.5 bottom-2.5 bg-[#002172] hover:bg-[#e33226] focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 "
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+
+        {/* for cards */}
       <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
-        {properties.map((property) => <DynamicCards key={property._id} property={property}></DynamicCards>)}
+         
+        {properties.map((property) => (
+          <DynamicCards key={property._id} property={property}></DynamicCards>
+        ))}
       </div>
     </div>
   );
