98c47f7de06d3e7cea01ccaf58b3e682086443d3
Tanbir Haque <tanbirhaque53@gmail.com>
2024-02-11 15:54:07 +0600

    Dashboard sidebar responsiveness done by Tanbir

--- src/DashLayout/DashLayout.jsx
+++ src/DashLayout/DashLayout.jsx
@@ -18,13 +18,13 @@ const DashLayout = () => {
       <div className="flex">
         {/* Sidebar of dashboard starts here */}
         <div
-          className={`side_bar bg-[#0f172a] min-h-screen fixed w-64  text-white transition-all duration-300 ${isSidebarOpen ? "" : "-ml-64"
-            }`}
+          className={`side_bar bg-[#0f172a] min-h-screen fixed w-64 z-[99999] text-white transition-all duration-300 ${isSidebarOpen ? "-ml-64 lg:ml-0" : "lg:-ml-64"
+        }`}
         >
           <SideBar></SideBar>
         </div>
         {/* Main content of Dashboard starts here */}
-        <div className={`main_content w-svw transition-all duration-300 ${isSidebarOpen ? "ml-64" : ""}`}>
+        <div className={`main_content w-svw transition-all duration-300 ${isSidebarOpen ? "lg:ml-64" : ""}`}>
           <DashNav toggleSidebar={toggleSidebar} ></DashNav>
           <div className="flex-1">
             <Outlet></Outlet>
