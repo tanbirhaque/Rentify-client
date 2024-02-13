4c78396e872366054244b67e2dfc2bf18e614ef4
sadiaafrin67 <sa540276@gmail.com>
2024-02-13 14:41:11 +0600

    all property section modified by sadia

--- src/MainLayout/Pages/Home/HomeComponents/AllProperties/AllProperties.jsx
+++ src/MainLayout/Pages/Home/HomeComponents/AllProperties/AllProperties.jsx
@@ -163,7 +163,7 @@ const AllProperties = () => {
    </div>
 
       {/* cards section */}
-      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-16">
         {/* {properties.map((property) => (
           <DynamicCards key={property._id} property={property}></DynamicCards>
         ))} */}
