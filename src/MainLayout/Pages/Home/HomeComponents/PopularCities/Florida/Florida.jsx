58b5341eae91cfc2bf348cd31f4511f1c7f926e1
sadiaafrin67 <sa540276@gmail.com>
2024-02-13 18:10:46 +0600

    add sort functinality on florida city dynamic route by sadia

--- src/MainLayout/Pages/Home/HomeComponents/PopularCities/Florida/Florida.jsx
+++ src/MainLayout/Pages/Home/HomeComponents/PopularCities/Florida/Florida.jsx
@@ -1,12 +1,65 @@
 import { NavLink } from "react-router-dom";
 import DynamicCards from "../DynamicCards";
 import useProperties from "../../../../../../Hooks/useProperties";
+import { useState } from "react";
 
 
 const Florida = () => {
 
     const [properties] = useProperties();
     const cityProperties = properties.filter(property => property?.property_info.property_location.address.city === 'Florida')
+    const [property, setProperty] = useState([]);
+    const [tabIndex, setTabIndex] = useState(0);
+  
+    const [countData, setCountData] = useState(cityProperties.length);
+  
+    const apartment = cityProperties.filter(
+      (item) => item.property_info.property_category === "Apartment"
+    );
+    const Commercial = cityProperties.filter(
+      (item) => item.property_info.property_category === "Commercial"
+    );
+    const Residential = cityProperties.filter(
+      (item) => item.property_info.property_category === "Residential"
+    );
+
+    const handleAllProperties = () => {
+        const count = cityProperties.length;
+        setCountData(count);
+      };
+    
+      const handleApartmentProperties = () => {
+        const count = apartment.length;
+        setCountData(count);
+      };
+    
+      const handleCommercialProperties = () => {
+        const count = Commercial.length;
+        setCountData(count);
+      };
+    
+      const handleResidentialProperties = () => {
+        const count = Residential.length;
+        setCountData(count);
+      };
+    
+      const sortByPriceLowToHigh = () => {
+        const sorted = [...properties].sort(
+          (a, b) =>
+            a.property_info.property_details.property_price -
+            b.property_info.property_details.property_price
+        );
+        setProperty(sorted);
+      };
+    
+      const sortByPriceHighToLow = () => {
+        const sorted = [...properties].sort(
+          (a, b) =>
+            b.property_info.property_details.property_price -
+            a.property_info.property_details.property_price
+        );
+        setProperty(sorted);
+      };
 
     return (
         <div>
--- src/MainLayout/Pages/Home/HomeComponents/PopularCities/Prauge/Prauge.jsx
+++ src/MainLayout/Pages/Home/HomeComponents/PopularCities/Prauge/Prauge.jsx
@@ -7,85 +7,98 @@ import PropertyCard from "../../../../../Shared/PropertyCards/PropertyCard";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
-
 const Prauge = () => {
+  const [properties] = useProperties();
+  const cityProperties = properties.filter(
+    (property) =>
+      property?.property_info.property_location.address.city === "Prauge"
+  );
+  const [property, setProperty] = useState([]);
+  const [tabIndex, setTabIndex] = useState(0);
 
-    const [properties] = useProperties();
-    const cityProperties = properties.filter(property => property?.property_info.property_location.address.city === 'Prauge')
-    const [property, setProperty] = useState([]);
-    const [tabIndex, setTabIndex] = useState(0);
-  
-    const [countData, setCountData] = useState(cityProperties.length);
+  const [countData, setCountData] = useState(cityProperties.length);
 
-    const apartment = cityProperties.filter(
-        (item) => item.property_info.property_category === "Apartment"
-      );
-      const Commercial = cityProperties.filter(
-        (item) => item.property_info.property_category === "Commercial"
-      );
-      const Residential = cityProperties.filter(
-        (item) => item.property_info.property_category === "Residential"
-      );
-    
-      const handleAllProperties = () => {
-        const count = cityProperties.length;
-        setCountData(count);
-      };
-    
-      const handleApartmentProperties = () => {
-        const count = apartment.length;
-        setCountData(count);
-      };
-    
-      const handleCommercialProperties = () => {
-        const count = Commercial.length;
-        setCountData(count);
-      };
-    
-      const handleResidentialProperties = () => {
-        const count = Residential.length;
-        setCountData(count);
-      };
-    
-      const sortByPriceLowToHigh = () => {
-        const sorted = [...properties].sort(
-          (a, b) =>
-            a.property_info.property_details.property_price -
-            b.property_info.property_details.property_price
-        );
-        setProperty(sorted);
-      };
-    
-      const sortByPriceHighToLow = () => {
-        const sorted = [...properties].sort(
-          (a, b) =>
-            b.property_info.property_details.property_price -
-            a.property_info.property_details.property_price
-        );
-        setProperty(sorted);
-      };
+  const apartment = cityProperties.filter(
+    (item) => item.property_info.property_category === "Apartment"
+  );
+  const Commercial = cityProperties.filter(
+    (item) => item.property_info.property_category === "Commercial"
+  );
+  const Residential = cityProperties.filter(
+    (item) => item.property_info.property_category === "Residential"
+  );
 
-    return (
-        <div>
-        {/* banner section */}
-        <div className="img2">
-                <div className=" bg-[#000000B2]">
-                    <div className=" max-w-screen-2xl mx-auto py-24">
-                        <h2 className="text-6xl font-bold text-white font-serif">All Properties in Prauge</h2>
-                        <div className="mt-2">
-                            <NavLink to='/' className='navAfter relative font-medium text-base text-white mx-3'>Home</NavLink>
-                            <NavLink to='/blogs' className='navAfter relative font-medium text-base text-white mx-3'>Blogs</NavLink>
-                           
-                        </div>
-                    </div>
-                </div>
+  const handleAllProperties = () => {
+    const count = cityProperties.length;
+    setCountData(count);
+  };
+
+  const handleApartmentProperties = () => {
+    const count = apartment.length;
+    setCountData(count);
+  };
+
+  const handleCommercialProperties = () => {
+    const count = Commercial.length;
+    setCountData(count);
+  };
+
+  const handleResidentialProperties = () => {
+    const count = Residential.length;
+    setCountData(count);
+  };
+
+  const sortByPriceLowToHigh = () => {
+    const sorted = [...properties].sort(
+      (a, b) =>
+        a.property_info.property_details.property_price -
+        b.property_info.property_details.property_price
+    );
+    setProperty(sorted);
+  };
+
+  const sortByPriceHighToLow = () => {
+    const sorted = [...properties].sort(
+      (a, b) =>
+        b.property_info.property_details.property_price -
+        a.property_info.property_details.property_price
+    );
+    setProperty(sorted);
+  };
+
+  return (
+    <div>
+      {/* banner section */}
+      <div className="img2">
+        <div className=" bg-[#000000B2]">
+          <div className=" max-w-screen-2xl mx-auto py-24">
+            <h2 className="text-6xl font-bold text-white font-serif">
+              All Properties in Prauge
+            </h2>
+            <div className="mt-2">
+              <NavLink
+                to="/"
+                className="navAfter relative font-medium text-base text-white mx-3"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/blogs"
+                className="navAfter relative font-medium text-base text-white mx-3"
+              >
+                Blogs
+              </NavLink>
             </div>
-            {/* <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+          </div>
+        </div>
+      </div>
+      {/* tab card section */}
+      {/* <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
               {
                 cityProperties?.map((property) => <DynamicCards key={property._id} property={property}></DynamicCards>)
               }
             </div> */}
-            <div className="max-w-[1296px] mx-auto  mb-[100px]">
+      <div className="max-w-[1296px] mx-auto  mb-[100px]">
         <div className="flex justify-between ">
           <p className="pt-16 font-bold text-lg text-gray-700">
             {countData} Results Found
@@ -223,20 +236,20 @@ const Prauge = () => {
                       ></DynamicCards>
                     ))
                   : apartment.map((property, index) => (
-                    <div key={index}>
-                      <PropertyCard
-                        property={property}
-                        details_path={"/details"}
-                      />
-                    </div>
-                  ))}
+                      <div key={index}>
+                        <PropertyCard
+                          property={property}
+                          details_path={"/details"}
+                        />
+                      </div>
+                    ))}
               </div>
             </TabPanel>
           </Tabs>
         </div>
       </div>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Prauge;
