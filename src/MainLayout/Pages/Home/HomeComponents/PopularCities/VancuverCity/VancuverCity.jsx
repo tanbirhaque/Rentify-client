a59b40b9d55de90ae0264062ddaecb98dc20f8db
sadiaafrin67 <sa540276@gmail.com>
2024-02-13 19:52:28 +0600

    fix the vancuver dynamic card route on popular cities section by sadia

--- src/MainLayout/Pages/Home/HomeComponents/PopularCities/VancuverCity/VancuverCity.jsx
+++ src/MainLayout/Pages/Home/HomeComponents/PopularCities/VancuverCity/VancuverCity.jsx
@@ -1,12 +1,68 @@
 import { NavLink } from "react-router-dom";
 import DynamicCards from "../DynamicCards";
 import useProperties from "../../../../../../Hooks/useProperties";
+import { useState } from "react";
+import PropertyCard from "../../../../../Shared/PropertyCards/PropertyCard";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 
 const VancuverCity = () => {
 
     const [properties] = useProperties();
     const cityProperties = properties.filter(property => property?.property_info.property_location.address.city === 'Vancuver City')
+    const [property, setProperty] = useState([]);
+    const [tabIndex, setTabIndex] = useState(0);
+  
+    const [countData, setCountData] = useState(cityProperties.length);
+  
+    const apartment = cityProperties.filter(
+      (item) => item.property_info.property_category === "Apartment"
+    );
+    const Commercial = cityProperties.filter(
+      (item) => item.property_info.property_category === "Commercial"
+    );
+    const Residential = cityProperties.filter(
+      (item) => item.property_info.property_category === "Residential"
+    );
+  
+    const handleAllProperties = () => {
+      const count = cityProperties.length;
+      setCountData(count);
+    };
+  
+    const handleApartmentProperties = () => {
+      const count = apartment.length;
+      setCountData(count);
+    };
+  
+    const handleCommercialProperties = () => {
+      const count = Commercial.length;
+      setCountData(count);
+    };
+  
+    const handleResidentialProperties = () => {
+      const count = Residential.length;
+      setCountData(count);
+    };
+  
+    const sortByPriceLowToHigh = () => {
+      const sorted = [...properties].sort(
+        (a, b) =>
+          a.property_info.property_details.property_price -
+          b.property_info.property_details.property_price
+      );
+      setProperty(sorted);
+    };
+  
+    const sortByPriceHighToLow = () => {
+      const sorted = [...properties].sort(
+        (a, b) =>
+          b.property_info.property_details.property_price -
+          a.property_info.property_details.property_price
+      );
+      setProperty(sorted);
+    };
 
     return (
         <div>
@@ -23,11 +79,132 @@ const VancuverCity = () => {
                     </div>
                 </div>
             </div>
-            <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
+            {/* <div className="max-w-screen-2xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
               {
                 cityProperties?.map((property) => <DynamicCards key={property._id} property={property}></DynamicCards>)
               }
-            </div>
+            </div> */}
+             {/* tab card section */}
+      <div className="max-w-[1296px] mx-auto  mb-[100px]">
+        <div className="flex justify-between ">
+          <p className="pt-16 font-bold text-lg text-gray-700">
+            {countData} Results Found
+          </p>
+
+          <div className=" py-10   ">
+            <details className="dropdown mb-10 ">
+              <summary className="m-1 btn bg-[#002172] text-white hover:bg-[#e33226]">
+                Sort By Price{" "}
+                <MdKeyboardArrowDown className="text-xl"></MdKeyboardArrowDown>
+              </summary>
+              <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+                <li>
+                  <a className="font-bold" onClick={sortByPriceHighToLow}>
+                    High to Low
+                  </a>
+                </li>
+                <li>
+                  <a className="font-bold" onClick={sortByPriceLowToHigh}>
+                    Low to High
+                  </a>
+                </li>
+              </ul>
+            </details>
+          </div>
+        </div>
+
+        <div className="">
+          <Tabs
+            defaultIndex={tabIndex}
+            onSelect={(index) => setTabIndex(index)}
+          >
+            <TabList className="ml-[2px]">
+              <Tab onClick={handleAllProperties}>All Properties</Tab>
+              <Tab onClick={handleResidentialProperties}>Residential</Tab>
+              <Tab onClick={handleCommercialProperties}>Commercial</Tab>
+              <Tab onClick={handleApartmentProperties}>Apartment</Tab>
+            </TabList>
+            <TabPanel>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 xl:px-0">
+              
+
+                {property.length > 0
+                  ? property.map((property) => (
+                      <DynamicCards
+                        key={property._id}
+                        property={property}
+                      ></DynamicCards>
+                    ))
+                  :  cityProperties?.map((property) => <DynamicCards key={property._id} property={property}></DynamicCards>)}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 xl:px-0">
+               
+
+                {property.length > 0
+                  ? property.map((property) => (
+                      <DynamicCards
+                        key={property._id}
+                        property={property}
+                      ></DynamicCards>
+                    ))
+                  : Residential.map((property, index) => (
+                      <div key={index}>
+                        <PropertyCard
+                          property={property}
+                          details_path={"/details"}
+                        />
+                      </div>
+                    ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 xl:px-0">
+               
+
+                {property.length > 0
+                  ? property.map((property) => (
+                      <DynamicCards
+                        key={property._id}
+                        property={property}
+                      ></DynamicCards>
+                    ))
+                  : Commercial.map((property, index) => (
+                      <div key={index}>
+                        <PropertyCard
+                          property={property}
+                          details_path={"/details"}
+                        />
+                      </div>
+                    ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-3 xl:px-0">
+                
+                  
+
+                {property.length > 0
+                  ? property.map((property) => (
+                      <DynamicCards
+                        key={property._id}
+                        property={property}
+                      ></DynamicCards>
+                    ))
+                  : apartment.map((property, index) => (
+                      <div key={index}>
+                        <PropertyCard
+                          property={property}
+                          details_path={"/details"}
+                        />
+                      </div>
+                    ))}
+              </div>
+            </TabPanel>
+          </Tabs>
+        </div>
+      </div>
         </div>
     );
 };
