9339c383c864166893c6cb0d5f6ebf8e0b59d0e7
Fahima <sfahima1388@gmail.com>
2024-02-28 09:30:50 +0600

    owners route made responsive by Fahima

--- src/MainLayout/Pages/Owner/OwnerList.jsx
+++ src/MainLayout/Pages/Owner/OwnerList.jsx
@@ -17,36 +17,37 @@ const OwnerList = () => {
   console.log(owners);
 
   return (
-   <>
-   <Helmet>
+    <>
+      <Helmet>
         <title>Rentify | Owners</title>
-        
-    </Helmet>
-    <div>
-      <div
-        className="hero"
-        style={{
-          backgroundImage: "url(https://i.ibb.co/Zdk0mLj/breadcrumb-4.jpg)",
-        }}
-      >
-        <div className="hero-overlay bg-[#000000c2] p-20">
-          <h2 className="mb-5 text-2xl md:text-[40px] font-bold text-white">Owner List</h2>
-          <p className="text-white">
-            <Link to="/" className="hover:text-[#ec3323] cursor-pointer">
-              Home
-            </Link>
-            / <span className="text-[#ec3323]"> Owners</span>
-          </p>
+      </Helmet>
+      <div>
+        <div
+          className="hero"
+          style={{
+            backgroundImage: "url(https://i.ibb.co/Zdk0mLj/breadcrumb-4.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-[#000000c2] p-20">
+            <h2 className="mb-5 text-2xl md:text-[40px] font-bold text-white">
+              Owner List
+            </h2>
+            <p className="text-white">
+              <Link to="/" className="hover:text-[#ec3323] cursor-pointer">
+                Home {""}
+              </Link>
+              / <span className="text-[#ec3323]"> Owners</span>
+            </p>
+          </div>
+        </div>
+        <div className="p-20 grid grid-rows-1 sm:grid-cols-2 md:grid-cols-3 gap-5 justify-center items-center">
+          {/* agents */}
+          {owners.map((owner) => (
+            <OwnerCard owner={owner} key={owner._id} />
+          ))}
         </div>
       </div>
-      <div className="p-20 grid grid-rows-1 md:grid-cols-3 gap-8">
-        {/* agents */}
-        {owners.map((owner) => (
-          <OwnerCard owner={owner} key={owner._id} />
-        ))}
-      </div>
-    </div>
-   </>
+    </>
   );
 };
 
