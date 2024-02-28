df62d85c707318f676c088a5f7155ff4003823b3
sadiaafrin67 <sa540276@gmail.com>
2024-02-28 20:58:03 +0600

    fix the responsive issue on footer 1024 size

--- src/MainLayout/Shared/Footer/Footer.jsx
+++ src/MainLayout/Shared/Footer/Footer.jsx
@@ -28,24 +28,18 @@ const Footer = () => {
             zIndex: 1000,
           }}
         >
-          <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-            <div className="w-[500px] flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+          <div className="container px-5 py-10 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+            <div className="w-[300px] flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
               <a className="flex title-font font-medium items-center md:justify-start justify-center text-white-900">
                 <img src={logo} alt="" />
                 <span className="font-bold text-white poppins-font text-[38px]">
                   Renti<span className="">fy</span>
                 </span>
               </a>
-              <p className="mt-2 lg:text-lg text-sm font-medium text-white py-5">
-                Rentify is one of the best and trusted real estate service
-                provider for rent and sale property. Rentify is the future of
-                real estate, offering a secure and dynamic platform with live
-                features for seamless transactions. Beyond listings, it fosters
-                a community-driven experience, making property transactions
-                engaging and insightful. With ongoing innovations, Rentify is
-                set to redefine and elevate the entire real estate journey.
+              <p className="mt-2  text-sm font-medium text-white py-5 ">
+             Discover seamless living with Rebtify – your premier destination for hassle-free online rentals. Browse a curated selection of top-quality homes, apartments,and more. 
               </p>
-              <h2 className="pt-5 text-2xl font-bold">Language</h2>
+              <h2 className=" text-xl font-bold">Language</h2>
               <details className="dropdown py-3">
                 <summary className="m-1 btn bg-blue-900 text-white border-none text-base hover:bg-blue-900">
                   English <IoIosArrowDown className="text-base" />
@@ -60,110 +54,115 @@ const Footer = () => {
                 </ul>
               </details>
             </div>
+            {/* links */}
             <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+              
+              {/* city links */}
               <div className="lg:w-1/3 md:w-1/2 w-full px-4">
                 <h2 className="title-font font-bold text-white tracking-widest text-2xl mb-3 ">
                   Popular Cities
                 </h2>
                 <hr className="w-14  md:mx-0 mx-auto linel" />
                 <nav className="list-none mb-10 pt-3 ">
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       New York City
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Prague
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       San Francisco
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Florida
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Los Angles
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Toronto City
                     </a>
                   </li>
                 </nav>
               </div>
+              {/* quick links */}
               <div className="lg:w-1/3 md:w-1/2 w-full px-4">
                 <h2 className="title-font font-bold text-white tracking-widest text-2xl mb-3 ">
                   Quick Links
                 </h2>
                 <hr className="w-14  md:mx-0 mx-auto linel" />
                 <nav className="list-none mb-10 pt-3 ">
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       About Us
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Agents
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Latest News
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Contact Us
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Privacy Policy
                     </a>
                   </li>
-                  <li className="items-center flex py-5 gap-3">
+                  <li className="items-center flex py-2 gap-3">
                     {" "}
-                    <IoIosArrowForward className="text-2xl text-[#e33226] font-bold" />
-                    <a className="text-white hover:text-[#e33226] text-xl  font-medium cursor-pointer">
+                    <IoIosArrowForward className="text-xl text-[#e33226] font-bold" />
+                    <a className="text-white hover:text-[#e33226] text-lg  font-medium cursor-pointer">
                       Terms & Conditions
                     </a>
                   </li>
                 </nav>
               </div>
+              {/* map */}
               <div className="lg:w-1/3 md:w-1/2 w-full px-4">
                 <iframe
-                  className="md:w-[350px] md:h-[450px] w-full h-full"
+                  className="md:max-w-[350px] md:h-[310px] w-full h-full"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7305.626951493346!2d90.4127511113167!3d23.71835406441302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8539c8e1dd7%3A0xf454a7a2dc22ba5!2sWari%2C%20Dhaka!5e0!3m2!1sen!2sbd!4v1708683809251!5m2!1sen!2sbd"
                   style={{ border: 0 }}
                   allowFullScreen
@@ -174,24 +173,25 @@ const Footer = () => {
             </div>
           </div>
           <hr className="container  mx-auto" />
-          <div className="bg-white-100 py-3">
-            <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-              <p className="text-white text-lg text-center sm:text-left font-semibold">
+          {/* ending */}
+          <div className="bg-white-100 py-2">
+            <div className="container mx-auto py-2 px-5 flex flex-wrap flex-col sm:flex-row">
+              <p className="text-white text-base text-center sm:text-left font-semibold">
                 © 2024 Rentify —
                 <a
                   href="https://twitter.com/knyttneve"
                   rel="noopener noreferrer"
-                  className="text-white ml-1 text-lg"
+                  className="text-white ml-1 text-base"
                   target="_blank"
                 >
                   All rights reserved at{" "}
-                  <span className="text-white font-bold text-lg">
+                  <span className=" text-[#e33226] font-bold text-lg">
                     @rentify.com
                   </span>
                 </a>
               </p>
-              <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                <a className="" href="https://www.facebook.com" target=" ">
+              <span className="inline-flex sm:ml-auto sm:mt-0  justify-center sm:justify-start">
+                <a className="hover:text-[#e33226]" href="https://www.facebook.com" target=" ">
                   <svg
                     fill="currentColor"
                     strokeLinecap="round"
@@ -204,7 +204,7 @@ const Footer = () => {
                   </svg>
                 </a>
                 <a
-                  className="ml-3 "
+                  className="ml-3 hover:text-[#e33226]"
                   href="https://twitter.com/home?lang=en"
                   target=" "
                 >
@@ -220,7 +220,7 @@ const Footer = () => {
                   </svg>
                 </a>
                 <a
-                  className="ml-3 "
+                  className="ml-3 hover:text-[#e33226]"
                   href="https://www.instagram.com"
                   target=" "
                 >
@@ -238,7 +238,7 @@ const Footer = () => {
                   </svg>
                 </a>
                 <a
-                  className="ml-3 "
+                  className="ml-3 hover:text-[#e33226]"
                   href="https://www.linkedin.com/feed"
                   target=" "
                 >
