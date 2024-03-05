b718bf28fa53d8d6abeaece9aa3836290a0a1f43
Tanbir Haque <tanbirhaque53@gmail.com>
2024-03-06 05:27:02 +0600

    Updated code from tanbir

--- src/DashLayout/DashboardRoutes/SavedProperties/SavedProperties.jsx
+++ src/DashLayout/DashboardRoutes/SavedProperties/SavedProperties.jsx
@@ -4,6 +4,8 @@ import SavedCards from "./SavedCards";
 import useSavedProperties from "../../../Hooks/useSavedProperties";
 import { useContext } from "react";
 import { AuthContext } from "../../../Provider/AuthProvider";
+import PageBanner from "../../../MainLayout/Shared/banner for pages/PageBanner";
+import savedBannerImg from "../../../assets/home-1/property-1.jpg"
 
 const SavedProperties = () => {
   const { user, loading } = useContext(AuthContext);
@@ -14,8 +16,8 @@ const SavedProperties = () => {
     return <progress className="progress w-56"></progress>;
   }
   return (
-    <div>
-      <div className="savedbgimg">
+    <div className="bg-[#f3f3f3]">
+      {/* <div className="savedbgimg">
         <div className=" bg-[#000000B2] rounded-lg">
           <div className=" max-w-screen-2xl mx-auto py-24 w-[1350px] ">
             <h2 className="text-6xl font-bold text-white font-serif pl-4">
@@ -26,8 +28,11 @@ const SavedProperties = () => {
             </p>
           </div>
         </div>
+      </div> */}
+      <div>
+        <PageBanner heading={"Saved Properties"} img={savedBannerImg} title={"Dashboard"}></PageBanner>
       </div>
-      <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+      <div className="my-10 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-y-10 gap-x-8  px-5 lg:px-16">
         {saved.map((item) => (
           <SavedCards key={item._id} item={item} refetch={refetch}></SavedCards>
         ))}
